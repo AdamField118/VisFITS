24aaafd9b214641b2a7e3df46a22d4122c46862c
AdamField118 <adfield@wpi.edu>
2025-05-02 20:28:21 -0400

    trying to fix the coadd to be centered in the right spot

--- VisFITS.py
+++ VisFITS.py
@@ -4,81 +4,79 @@ from astropy.visualization import ImageNormalize, ZScaleInterval
 from astropy.wcs import WCS
 from argparse import ArgumentParser
 import os
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 def parse_args():
-
     parser = ArgumentParser()
-
-    parser.add_argument('outdir', type=str, default=None,
-                        help='Output directory for images')
-    parser.add_argument('coaddpath', type=str, default=None,
-                        help='Path to COADD File')
-    parser.add_argument('emodepath', type=str, default=None,
-                        help='Path to COADD File')
-    parser.add_argument('cluster_name', type=str, default=None,
-                        help='Path to COADD File')
-    parser.add_argument('band_name', type=str, default=None,
-                        help='Path to COADD File')
-
+    parser.add_argument('outdir', type=str, help='Output directory for images')
+    parser.add_argument('coaddpath', type=str, help='Path to COADD File')
+    parser.add_argument('emodepath', type=str, help='Path to EMODE File')
+    parser.add_argument('cluster_name', type=str, help='Cluster name')
+    parser.add_argument('band_name', type=str, help='Band name')
     return parser.parse_args()
 
-def main(args, path, name):
-    outdir = args.outdir
-    cluster_name = args.cluster_name
-    band_name = args.band_name
+def get_reference_coords(emode_path):
+    """Get center coordinates from emode FITS header"""
+    with fits.open(emode_path) as hdul:
+        header = hdul[0].header
+        return SkyCoord(header['CRVAL1'] * u.deg, 
+                       header['CRVAL2'] * u.deg, 
+                       frame='icrs')
 
+def process_image(args, path, name, target_coord):
     hdul = fits.open(path)
     image_data = hdul[0].data
-    header = hdul[0].header
-
+    header = hdul[0].header.copy()
+    
+    # Force consistent projection
     header['CTYPE1'] = 'RA---TAN'
     header['CTYPE2'] = 'DEC--TAN'
-
     wcs = WCS(header)
+    
+    # Convert target coordinates to this image's pixel space
+    target_x, target_y = wcs.world_to_pixel(target_coord)
+    
+    crop_range = 500
+    height, width = image_data.shape
+    
+    # Calculate safe crop boundaries
+    y_start = max(0, int(target_y) - crop_range)
+    y_end = min(height, int(target_y) + crop_range)
+    x_start = max(0, int(target_x) - crop_range)
+    x_end = min(width, int(target_x) + crop_range)
+    
+    cropped_data = image_data[y_start:y_end, x_start:x_end]
 
-    norm = ImageNormalize(image_data, interval=ZScaleInterval())
-
+    # Create plot
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection=wcs)
+    norm = ImageNormalize(cropped_data, interval=ZScaleInterval())
+    im = ax.imshow(cropped_data, cmap='magma', norm=norm, origin='lower')
 
-    center_x = int(header['CRPIX1'])
-    center_y = int(header['CRPIX2'])
-    range = 500
-
-    im = ax.imshow(image_data[(center_y-range):(center_y+range), (center_x-range):(center_x+range)], cmap='magma', norm=norm, origin='lower')
-
+    # Coordinate formatting
     ra = ax.coords['ra']
     dec = ax.coords['dec']
-
-    # International Celestial Reference System
     ra.set_axislabel('Right Ascension (ICRS)')
     dec.set_axislabel('Declination (ICRS)')
-
-    # Force tick parameters
     ra.set_ticks(size=10, color='white', width=1.5)
     dec.set_ticks(size=10, color='white', width=1.5)
-
-    # Format ticks in sexagesimal
     ra.set_major_formatter('hh:mm:ss')
     dec.set_major_formatter('dd:mm:ss')
-
-    # Add grid
     ax.coords.grid(True, color='white', linestyle='--', alpha=0.7)
-
-    # Astronomical convention
     ax.invert_xaxis()
 
-
-    # Janksy (Jy) unit of flux density: 1 Jy = 10^{-26} W/m^{2}/Hz
-    # beam is the effective resolution element of the telescope
-    # Jy/beam is the flux density per beam area (common unit in radio/mm astronomy)
     plt.colorbar(im, pad=0.15).set_label('Flux (Jy/beam)')
-    plt.savefig(os.path.join(outdir, f"{cluster_name}_{band_name}_{name}.png"))
+    plt.savefig(os.path.join(args.outdir, f"{args.cluster_name}_{args.band_name}_{name}.png"))
+    plt.close()
     hdul.close()
 
 if __name__ == '__main__':
     args = parse_args()
-    coaddpath = args.coaddpath
-    emodepath = args.emodepath
-    main(args, coaddpath, 'coadd')
-    main(args, emodepath, 'emode')
+    
+    # Get reference coordinates from emode
+    target_coord = get_reference_coords(args.emodepath)
+    
+    # Process both images using the same reference coordinates
+    process_image(args, args.coaddpath, 'coadd', target_coord)
+    process_image(args, args.emodepath, 'emode', target_coord)
